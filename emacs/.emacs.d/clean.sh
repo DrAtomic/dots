27bc063abccb83f797cf60e06fa6213f53c68e79
DrAtomic <leo.ward.garcia@gmail.com>
2022-09-24 17:27:57 -0700

    add cleaning script

---
+++ emacs/.emacs.d/clean.sh
@@ -0,0 +1,5 @@
+set -xe
+
+rm -rf auto-save-list init.el config.el org-roam.db projectile-bookmarks.eld recentf elpa/ .dap-breakpoints .lsp-session-v1 .org-id-locations .cache 
+
+git restore init.el
