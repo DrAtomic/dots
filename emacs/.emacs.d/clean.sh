6c01def247f7df07b636a21f39c6bcc30732d988
DrAtomic <leo.ward.garcia@gmail.com>
2022-09-24 19:46:04 -0700

    updated clean script

--- emacs/.emacs.d/clean.sh
+++ emacs/.emacs.d/clean.sh
@@ -1,5 +1,5 @@
 set -xe
 
-rm -rf auto-save-list init.el config.el org-roam.db projectile-bookmarks.eld recentf elpa/ .dap-breakpoints .lsp-session-v1 .org-id-locations .cache 
+rm -rf auto-save-list init.el config.el org-roam.db projectile-bookmarks.eld recentf elpa/ .dap-breakpoints .lsp-session-v1 .org-id-locations .cache snippets transient
 
 git restore init.el
